384286ce9580c514d584a0c9b112d2fb87a0fb72
Hanan Hussain <hhussain141003@gmail.com>
2025-02-27 08:50:21 +0000

    Loaded llm model and data

--- model.py
+++ model.py
@@ -9,3 +9,7 @@ with open("document_store.json", "r", encoding="utf-8") as f:
     document_store = json.load(f)
 
 embedder = SentenceTransformer("./models/all-MiniLM-L6-v2")
+
+model_name = "./models/deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5b"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto")
